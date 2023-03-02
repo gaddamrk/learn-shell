525472c67995d04f077c1cce54f580bcb94b228f
raj <raj>
2023-03-02 14:29:54 +0530

    common.sh

---
+++ special-vars.sh
@@ -0,0 +1,20 @@
+echo fallowing values from script
+echo "install nginx - $1"
+echo "start nginx - $2"
+echo "system reload - $3"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+
+echo fallwoing values from function
+example () {
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "3 - $3"
+echo "* - $*"
+echp "@ - $@"
+echo "# - $#"
+}
+
+example $*
