25cd2884d320901d391384990e0e08619a555240
raj <raj>
2023-03-02 12:25:22 +0530

    if data

---
+++ function .sh
@@ -0,0 +1,11 @@
+functions are like variables
+variables : when we assaign a name to a set of data then it is variables
+functions : when we assaign a name to a set of commands then it is function
+
+example () {
+  echo i am a function
+}
+
+call the function
+
+example
